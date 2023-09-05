a395101d4d54649db6d20cb41997b90f262a501b
sheva2103 <sheva2103@list.ru>
2023-09-05 20:05:18 +0300

    5.09.23

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+//import React, { Suspense } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
@@ -6,6 +6,8 @@ import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
 import store from './store';
 import { BrowserRouter } from 'react-router-dom';
+
+//import './i18n'
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
